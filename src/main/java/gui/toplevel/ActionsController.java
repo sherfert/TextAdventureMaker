0379ef09920522e337e9890830aa851b14ee1766
Satia Herfert <satiaherfert@gmx.de>
2016-09-27 10:52:38 +0200

    Progess on the ActionCreationWizard

--- src/main/java/gui/toplevel/ActionsController.java
+++ src/main/java/gui/toplevel/ActionsController.java
@@ -10,6 +10,8 @@ import java.util.logging.Logger;
 import org.controlsfx.dialog.Wizard;
 import org.controlsfx.dialog.WizardPane;
 
+import data.InventoryItem;
+import data.Location;
 import data.action.AbstractAction;
 import data.action.AddInventoryItemsAction;
 import data.action.ChangeActionAction;
@@ -30,6 +32,8 @@ import exception.DBClosedException;
 import gui.MainWindowController;
 import gui.NamedObjectsTableController;
 import gui.customui.ActionChooser;
+import gui.customui.InventoryItemChooser;
+import gui.customui.LocationChooser;
 import gui.utility.Loader;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.ObservableMap;
@@ -41,6 +45,7 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.text.Text;
 import logic.CurrentGameManager;
@@ -129,6 +134,19 @@ public class ActionsController extends NamedObjectsTableController<AbstractActio
 		return currentGameManager.getPersistenceManager().getActionManager().getAllActions();
 	}
 
+	// Constants for the wizard settings
+	private static final String TYPE_KEY = "type";
+	private static final String NAME_KEY = "name";
+	private static final String ACTIONTOCHANGE_KEY = "actionToChange";
+	private static final String REMOVEITEM_KEY = "removeItem";
+	private static final String MOVETARGET_KEY = "moveTarget";
+	
+	private static final String TYPE_REMOVEII = "removeII";
+	private static final String TYPE_ADDII = "addII";
+	private static final String TYPE_MULTI = "multi";
+	private static final String TYPE_ENDGAME = "endGame";
+	public static final Object TYPE_MOVE = "move";
+
 	/**
 	 * Opens a dialog to create a new action.
 	 */
@@ -137,9 +155,13 @@ public class ActionsController extends NamedObjectsTableController<AbstractActio
 		ChooseTypePane ctp = new ChooseTypePane();
 		ChooseActionToChangePane catcp = new ChooseActionToChangePane();
 		ChooseNamePane cnp = new ChooseNamePane();
+		ChooseRemoveItemPane crip = new ChooseRemoveItemPane();
+		ChooseMoveTargetPane cmtp = new ChooseMoveTargetPane();
 
-		NewActionWizardFlow flow = new NewActionWizardFlow(ctp, catcp, cnp);
+		NewActionWizardFlow flow = new NewActionWizardFlow(ctp, catcp, cnp, crip, cmtp);
 
+		// TODO own wizard subclass ?
+		// TODO wizard and dialog icons
 		// create wizard
 		Wizard wizard = new Wizard();
 		wizard.setTitle("New action");
@@ -152,13 +174,26 @@ public class ActionsController extends NamedObjectsTableController<AbstractActio
 			if (result == ButtonType.FINISH) {
 				AbstractAction newAction = null;
 
+				// TODO String constants
 				// Read settings and create an action accordingly
 				ObservableMap<String, Object> settings = wizard.getSettings();
 				// TODO remove sysout
 				System.out.println("Wizard finished, settings: " + settings);
-				if ((Boolean) settings.get("changeActionRB")) {
-					newAction = new ChangeActionAction((String) settings.get("name"),
-							(AbstractAction) settings.get("actionToChange"));
+				if (settings.get(TYPE_KEY).equals("changeAction")) {
+					newAction = new ChangeActionAction((String) settings.get(NAME_KEY),
+							(AbstractAction) settings.get(ACTIONTOCHANGE_KEY));
+				} else if (settings.get(TYPE_KEY).equals(TYPE_ENDGAME)) {
+					newAction = new EndGameAction((String) settings.get(NAME_KEY));
+				} else if (settings.get(TYPE_KEY).equals(TYPE_MULTI)) {
+					newAction = new MultiAction((String) settings.get(NAME_KEY));
+				} else if (settings.get(TYPE_KEY).equals(TYPE_ADDII)) {
+					newAction = new AddInventoryItemsAction((String) settings.get(NAME_KEY));
+				} else if (settings.get(TYPE_KEY).equals(TYPE_REMOVEII)) {
+					newAction = new RemoveInventoryItemAction((String) settings.get(NAME_KEY),
+							(InventoryItem) settings.get(REMOVEITEM_KEY));
+				} else if (settings.get(TYPE_KEY).equals(TYPE_MOVE)) {
+					newAction = new MoveAction((String) settings.get(NAME_KEY),
+							(Location) settings.get(MOVETARGET_KEY));
 				}
 
 				if (newAction != null) {
@@ -195,12 +230,17 @@ public class ActionsController extends NamedObjectsTableController<AbstractActio
 		private ChooseTypePane ctp;
 		private ChooseActionToChangePane catcp;
 		private ChooseNamePane cnp;
+		private ChooseRemoveItemPane crip;
+		private ChooseMoveTargetPane cmtp;
 
 		// Expects all panes
-		public NewActionWizardFlow(ChooseTypePane ctp, ChooseActionToChangePane catcp, ChooseNamePane cnp) {
+		public NewActionWizardFlow(ChooseTypePane ctp, ChooseActionToChangePane catcp, ChooseNamePane cnp,
+				ChooseRemoveItemPane crip, ChooseMoveTargetPane cmtp) {
 			this.ctp = ctp;
 			this.catcp = catcp;
 			this.cnp = cnp;
+			this.crip = crip;
+			this.cmtp = cmtp;
 		}
 
 		@Override
@@ -208,10 +248,16 @@ public class ActionsController extends NamedObjectsTableController<AbstractActio
 			if (currentPage == null) {
 				return Optional.of(ctp);
 			} else if (currentPage == ctp) {
-				if (ctp.typeTG.getSelectedToggle() == ctp.changeActionRB) {
+				if (ctp.changeActionRB.isSelected()) {
 					return Optional.of(catcp);
+				} else if (ctp.removeIIRB.isSelected()) {
+					return Optional.of(crip);
+				} else if (ctp.moveRB.isSelected()) {
+					return Optional.of(cmtp);
+				} else if (ctp.endGameRB.isSelected() || ctp.multiRB.isSelected() || ctp.addIIRB.isSelected()) {
+					return Optional.of(cnp);
 				}
-			} else if (currentPage == catcp) {
+			} else if (currentPage == catcp || currentPage == crip || currentPage == cmtp) {
 				return Optional.of(cnp);
 			}
 			// As default, don't switch
@@ -251,6 +297,27 @@ public class ActionsController extends NamedObjectsTableController<AbstractActio
 		public void onEnteringPage(Wizard wizard) {
 			wizard.setInvalid(false);
 		}
+
+		@Override
+		public void onExitingPage(Wizard wizard) {
+			if (changeUseWithRB.isSelected()) {
+				wizard.getSettings().put(TYPE_KEY, "changeUseWith");
+			} else if (changeCombineRB.isSelected()) {
+				wizard.getSettings().put(TYPE_KEY, "changeCombine");
+			} else if (changeActionRB.isSelected()) {
+				wizard.getSettings().put(TYPE_KEY, "changeAction");
+			} else if (addIIRB.isSelected()) {
+				wizard.getSettings().put(TYPE_KEY, TYPE_ADDII);
+			} else if (removeIIRB.isSelected()) {
+				wizard.getSettings().put(TYPE_KEY, TYPE_REMOVEII);
+			} else if (moveRB.isSelected()) {
+				wizard.getSettings().put(TYPE_KEY, TYPE_MOVE);
+			} else if (multiRB.isSelected()) {
+				wizard.getSettings().put(TYPE_KEY, TYPE_MULTI);
+			} else if (endGameRB.isSelected()) {
+				wizard.getSettings().put(TYPE_KEY, TYPE_ENDGAME);
+			}
+		}
 	}
 
 	/**
@@ -278,7 +345,66 @@ public class ActionsController extends NamedObjectsTableController<AbstractActio
 
 		@Override
 		public void onExitingPage(Wizard wizard) {
-			wizard.getSettings().put("actionToChange", actionChooser.getObjectValue());
+			wizard.getSettings().put(ACTIONTOCHANGE_KEY, actionChooser.getObjectValue());
+		}
+	}
+
+	/**
+	 * Pane to choose which item to remove for a
+	 * {@link RemoveInventoryItemAction}.
+	 * 
+	 * @author satia
+	 */
+	public class ChooseRemoveItemPane extends WizardPane {
+		private @FXML InventoryItemChooser removeItemChooser;
+		private Wizard wizard;
+
+		public ChooseRemoveItemPane() {
+			Loader.load(this, this, "view/wizards/CreateActionChooseRemoveItem.fxml");
+			removeItemChooser.initialize(null, false, false,
+					currentGameManager.getPersistenceManager().getInventoryItemManager()::getAllInventoryItems, (a) -> {
+						wizard.setInvalid(a == null);
+					});
+		}
+
+		@Override
+		public void onEnteringPage(Wizard wizard) {
+			wizard.setInvalid(removeItemChooser.getObjectValue() == null);
+			this.wizard = wizard;
+		}
+
+		@Override
+		public void onExitingPage(Wizard wizard) {
+			wizard.getSettings().put(REMOVEITEM_KEY, removeItemChooser.getObjectValue());
+		}
+	}
+
+	/**
+	 * Pane to choose target for a {@link MoveAction}.
+	 * 
+	 * @author satia
+	 */
+	public class ChooseMoveTargetPane extends WizardPane {
+		private @FXML LocationChooser moveTargetChooser;
+		private Wizard wizard;
+
+		public ChooseMoveTargetPane() {
+			Loader.load(this, this, "view/wizards/CreateActionChooseMoveTarget.fxml");
+			moveTargetChooser.initialize(null, false, false,
+					currentGameManager.getPersistenceManager().getLocationManager()::getAllLocations, (a) -> {
+						wizard.setInvalid(a == null);
+					});
+		}
+
+		@Override
+		public void onEnteringPage(Wizard wizard) {
+			wizard.setInvalid(moveTargetChooser.getObjectValue() == null);
+			this.wizard = wizard;
+		}
+
+		@Override
+		public void onExitingPage(Wizard wizard) {
+			wizard.getSettings().put(MOVETARGET_KEY, moveTargetChooser.getObjectValue());
 		}
 	}
 
@@ -295,7 +421,8 @@ public class ActionsController extends NamedObjectsTableController<AbstractActio
 			Loader.load(this, this, "view/wizards/CreateActionChooseName.fxml");
 			nameTF.textProperty().addListener((f, o, n) -> {
 				wizard.setInvalid(n.isEmpty());
-				wizard.getSettings().put("name", n);
+				// XXX cannot do this in onExitingPage: Not called for last page
+				wizard.getSettings().put(NAME_KEY, n);
 			});
 		}
 
