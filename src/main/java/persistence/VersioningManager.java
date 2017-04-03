58d8ea37ec492a13589f5dc301266fcdedee4054
Satia Herfert <satiaherfert@gmx.de>
2017-04-03 18:18:08 +0200

    Log message if DB is updated.

--- src/main/java/persistence/VersioningManager.java
+++ src/main/java/persistence/VersioningManager.java
@@ -1,5 +1,8 @@
 package persistence;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import exception.DBIncompatibleException;
 
 /**
@@ -39,12 +42,15 @@ public class VersioningManager {
 	 *             if the new version is older than the "old" version.
 	 */
 	public void updateDB(int fromMajor, int fromMinor, int toMajor, int toMinor) throws DBIncompatibleException {
-		if(toMajor < fromMajor || (toMajor == fromMajor && toMinor < fromMinor)) {
-			String msg = String.format("The loaded DB is too new (version %d.%d vs %d.%d). Consider updating TextAdventureMaker.",
+		if (toMajor < fromMajor || (toMajor == fromMajor && toMinor < fromMinor)) {
+			String msg = String.format(
+					"The loaded DB is too new (version %d.%d vs %d.%d). Consider updating TextAdventureMaker.",
 					fromMajor, fromMinor, toMajor, toMinor);
 			throw new DBIncompatibleException(msg);
 		}
 		// Add updating logic here.
+		Logger.getLogger(this.getClass().getName()).log(Level.INFO,
+				String.format("Updating the database model %d.%d -> %d.%d", fromMajor, fromMinor, toMajor, toMinor));
 	}
 
 }
